8039380edd8fad72dd060ed289429d55dc4afb7f
IsaacKlark <isaackeklarke@gmail.com>
2019-11-12 14:16:49 +0200

    solution

--- src/case-swapping.js
+++ src/case-swapping.js
@@ -15,7 +15,16 @@
  * @return {string}
  */
 function swapCase(phrase) {
-  // write code here
+  const phraseNormalize = phrase.toLowerCase();
+  const reverseCase = [];
+  for (let i = 0; i < phrase.length; i++) {
+    if (phraseNormalize[i] === phrase[i]) {
+      reverseCase.push(phrase[i].toUpperCase());
+    } else {
+      reverseCase.push(phrase[i].toLowerCase());
+    }
+  }
+  return reverseCase.join('');
 }
 
 module.exports = swapCase;
