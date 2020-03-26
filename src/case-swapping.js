97c2be53f14f13cd0fd0e2cf923cd0b288f73129
olenazubashenko <olenazubashenko@gmail.com>
2020-03-26 22:11:09 +0200

    solution

--- src/case-swapping.js
+++ src/case-swapping.js
@@ -15,7 +15,26 @@
  * @return {string}
  */
 function swapCase(phrase) {
-  // write code here
+  const arr = [];
+
+  for (let i = 0; i < phrase.length; i++) {
+    if (isFinite(phrase[i])) {
+      arr.push(phrase[i]);
+      continue;
+    }
+
+    if (phrase[i] === phrase[i].toLowerCase()) {
+      arr.push(phrase[i].toUpperCase());
+      continue;
+    }
+
+    if (phrase[i] === phrase[i].toUpperCase()) {
+      arr.push(phrase[i].toLowerCase());
+      continue;
+    }
+  }
+
+  return arr.join('');
 }
 
 module.exports = swapCase;
