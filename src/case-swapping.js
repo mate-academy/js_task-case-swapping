4f72e31450141ede7227bb3bb19b2799236ee44e
Tata2222 <tataolcha@gmail.com>
2020-03-27 22:02:03 +0200

    solution

--- src/case-swapping.js
+++ src/case-swapping.js
@@ -15,7 +15,17 @@
  * @return {string}
  */
 function swapCase(phrase) {
-  // write code here
+  let result = '';
+
+  for (const symbol of phrase) {
+    if (symbol === symbol.toLowerCase()) {
+      result += symbol.toUpperCase();
+    } else {
+      result += symbol.toLowerCase();
+    }
+  }
+
+  return result;
 }
 
 module.exports = swapCase;
