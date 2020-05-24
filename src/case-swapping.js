86b43b9ee6723f07597d8819eb6be32e85e7faf5
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-05-24 20:41:32 +0300

    develop

--- src/case-swapping.js
+++ src/case-swapping.js
@@ -15,7 +15,17 @@
  * @return {string}
  */
 function swapCase(phrase) {
-  // write code here
+  let swapPhrase = '';
+
+  for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i] === phrase[i].toUpperCase()) {
+      swapPhrase = swapPhrase + phrase[i].toLowerCase();
+    } else {
+      swapPhrase = swapPhrase + phrase[i].toUpperCase();
+    }
+  }
+
+  return swapPhrase;
 }
 
 module.exports = swapCase;
